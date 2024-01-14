3f8bc3acf90e04c23aeada62ad887863f1ecbeda
Raudin <markmuga72@gmail.com>
2024-01-14 18:09:54 +0300

    Updates

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="p-4 rounded-lg shadow md:flex md:items-center md:justify-between md:p-6">
-            <span className="text-sm text-white sm:text-center">© 2023 Movie App. All Rights Reserved.</span>
+            <span className="text-sm text-white sm:text-center">© {new Date().getFullYear()} Movie Buzz. All Rights Reserved.</span>
         </footer>
     );
 }
