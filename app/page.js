8599bc4d156566861b541f93eb06240b66e86c7b
Raudin <markmuga72@gmail.com>
2024-03-27 21:53:12 +0300

    Styled the "Load More" button

--- app/page.js
+++ app/page.js
@@ -35,11 +35,14 @@ export default function Home() {
           />
         ))}
       </div>
+      <br/>
+      <div className="flex justify-center items-center ">
       <button 
-      className = ""
+      className = "select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
       onClick={() => setCurrentPage((prevPage) => prevPage + 1)}>
         Load More
       </button>
+      </div>
     </main>
   );
 }
