0b601760ae298121bcfb12adad77af8559c7eede
Raudin <markmuga72@gmail.com>
2024-03-27 21:30:24 +0300

    Added the ability to load more movies

--- app/page.js
+++ app/page.js
@@ -1,14 +1,31 @@
-import Movie from "../components/Movie"
+"use client"
+import { useState, useEffect } from "react";
+import Movie from "../components/Movie";
 
-export default async function Home() {
-  const data = await fetch(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.NEXT_PUBLIC_API_KEY}`
-  );
-  const res = await data.json();
+export default function Home() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async () => {
+    const data = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.NEXT_PUBLIC_API_KEY}&page=${currentPage}`
+    );
+    const res = await data.json();
+    setMovies((prevMovies) => [...prevMovies, ...res.results]);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, [currentPage]);
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <main className="px-4 py-10 sm:px-6 lg:px-8">
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {res.results && res.results.map((movie) => (
+        {movies.map((movie) => (
           <Movie
             key={movie.id}
             id={movie.id}
@@ -18,6 +35,11 @@ export default async function Home() {
           />
         ))}
       </div>
+      <button 
+      className = ""
+      onClick={() => setCurrentPage((prevPage) => prevPage + 1)}>
+        Load More
+      </button>
     </main>
-  )
+  );
 }
